b094599dca809713fcf9b384669f4670776e8079
kingller.tang <kingller.tang@gaiaworks.cn>
2018-12-20 12:04:22 +0800

    Initial the page of project

---
+++ precache-manifest.f0da94b5d08310849911f0b5dd57583c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "854c0b713618c7476887",
+    "url": "./static/js/main.854c0b71.chunk.js"
+  },
+  {
+    "revision": "f242f0399c2d10faefc2",
+    "url": "./static/js/1.f242f039.chunk.js"
+  },
+  {
+    "revision": "854c0b713618c7476887",
+    "url": "./static/css/main.d5e997d5.chunk.css"
+  },
+  {
+    "revision": "e23e3beb20ffc176c46805d5dae0bb70",
+    "url": "./index.html"
+  }
+];
